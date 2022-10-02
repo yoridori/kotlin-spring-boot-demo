59690f7483234cf75d3e8c060253199481851c0c
yoridori <midori.engineer@gmail.com>
2022-10-02 12:48:55 +0900

    初期コミット

---
+++ src/test/kotlin/com/yoridori/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yoridori.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
