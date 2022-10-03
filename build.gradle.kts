6cb6295d5f96661339ddd7d0b4984c496b596f46
yoridori <midori.engineer@gmail.com>
2022-10-03 10:51:18 +0900

    MybatisGenerator導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.7.4"
 	id("io.spring.dependency-management") version "1.0.14.RELEASE"
+	id("com.thinkimi.gradle.MybatisGenerator") version "2.4"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 }
@@ -21,7 +22,16 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.mybatis:mybatis:3.5.11")
+	implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.4.0")
+	implementation("mysql:mysql-connector-java:8.0.30")
+	mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.1")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+mybatisGenerator {
+	verbose = true
+	configFile = "${projectDir}/src/main/resources/generatorConfig.xml"
 }
 
 tasks.withType<KotlinCompile> {
